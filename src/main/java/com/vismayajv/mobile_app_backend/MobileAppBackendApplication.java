c782f6cb07c04e2c0515fc0a9e17c6b58de890cf
Vismaya J V <jvvismaya@gmail.com>
2026-09-22 07:53:25 -0700

    Initial backend setup

---
+++ src/main/java/com/vismayajv/mobile_app_backend/MobileAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vismayajv.mobile_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vismayajv/mobile_app_backend/MobileAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vismayajv.mobile_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
